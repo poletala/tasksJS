73d74fd15d00dae654695b2837de9eb0e9e27b25
Alena <92329824+poletala@users.noreply.github.com>
2024-02-08 23:05:16 +0500

    task 4

---
+++ task4.js
@@ -0,0 +1,30 @@
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+let randomNumber = getRandomNumber(1, 10)
+console.log(randomNumber)
+
+
+function guessGame() {
+    let userNumber = prompt('Угадай число от 1 до 10');
+    if (userNumber > randomNumber) {
+        alert ('Загаданное число меньше')
+        guessGame()
+    }
+    else if (userNumber < randomNumber) {
+        alert ('Загаданное число больше')
+        guessGame()
+    }
+    else if (isNaN(userNumber)) {
+        alert ('Введи число!')
+        guessGame()
+    }
+    else if (userNumber === null || userNumber === '') {
+        alert ('Игра окончена')
+    }
+    else {alert("Поздравляю, Вы угадали!!!")
+    }
+}
+
+guessGame()
